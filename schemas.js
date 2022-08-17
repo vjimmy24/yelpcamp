6a7e62bee08da860235b9951bc1f5c2038a1f2cb
Jimmy Vu <92957140+vjimmy24@users.noreply.github.com>
2022-08-17 10:22:10 -0400

    Added review schema

--- schemas.js
+++ schemas.js
@@ -1,10 +1,19 @@
 const joi = require('joi');
 
 module.exports.campgroundSchema = joi.object({
-    title: joi.string().required(),
-    location: joi.string().required(),
-    price: joi.number().required().min(0),
-    description: joi.string().required(),
-    image: joi.string().required(),
+    camp: joi.object({
+        title: joi.string().required(),
+        location: joi.string().required(),
+        price: joi.number().required().min(0),
+        description: joi.string().required(),
+        // image: joi.string().required(),
+    }).required(),
+    deleteImages: joi.array()
+})
 
+module.exports.reviewSchema = joi.object({
+    review: joi.object({
+        rating: joi.string().required(),
+        body: joi.string().required()
+    }).required()
 })
